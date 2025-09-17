9165df8de3bf0beb882ee502b7815d1d27568d9f
Chris Clements <cclements1384@gmail.com>
2025-09-16 21:56:41 -0500

    Updated with training and evaulation code.

--- app.py
+++ app.py
@@ -7,16 +7,16 @@ import torchvision.transforms as transforms
 import matplotlib.pyplot as plt 
 
 # load the MNSIT dataset
-"""
-transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0,.5), (0, .0))])
+
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5), (0.5,))])
 
 trainset = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform)
 testset = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform)
 
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
 testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=False)
-"""
 
+"""
 transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
 
 trainset = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform)
@@ -24,6 +24,8 @@ testset = torchvision.datasets.MNIST(root='./data', train=False, download=True, 
 
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
 testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=False)
+
+"""
 
 #build the model (feed forward neural network with one hidden layer)
 class SimpleNN(nn.Module):
@@ -72,5 +74,32 @@ for epoc in range(num_epocs):
             print(f"[Epoc {epoc+1}, Batch {i+1}, Loss:{running_loss / 100:.4f}")
             running_loss = 0.0
     train_losses.append(running_loss/len(trainloader))
+
+
+# Plotting and Training the Validation Curves
+plt.plot(train_losses, label= "Training Loss")
+plt.title('Training Loss Curve')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+# Evaluate the model
+correct = 0
+total = 0
+
+model.eval()
+
+with torch.no_grad():
+    for images, labels in testloader:
+        if torch.cuda.is_available():
+            images, labels = images.cuda(), labels.cuda()
+        outputs = model(images)
+        _, predicted = torch.max(outputs, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+accuracy = 100 * correct / total
+print(f"Test Accuracy: {accuracy:.2f}%")
 
 print("Program Complete.")
